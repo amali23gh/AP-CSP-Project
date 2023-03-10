722af223f3415af3c5f2c0a895bf705e57ed6384
amali23gh <aa46632129@gmail.com>
2023-03-10 08:54:01 -0600

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Adam Ali23 on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
