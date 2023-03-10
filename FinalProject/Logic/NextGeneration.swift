9b10b75ae19707cbf39b0d2c64fee69d95d6115e
Russan-bot-9 <tpfechtmeye23@blakeschool.org>
2023-03-10 10:55:23 -0600

    ok i missed one thing

--- FinalProject/Logic/NextGeneration.swift
+++ FinalProject/Logic/NextGeneration.swift
@@ -5,7 +5,7 @@
 //  Created by Theo Fechtmeyer23 on 3/10/23.
 //
 
-import function
+import Foundation
 
 func NextGen(startPop: [[Int]], fertility: Int, monogamy: Bool) -> [[Int]]{
     var malePop: [[Int]] = []
