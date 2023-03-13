41198c5a6f4340b5634b8e414b13146738be89b8
Russan-bot-9 <tpfechtmeye23@blakeschool.org>
2023-03-12 22:09:02 -0500

    io8t9

--- FinalProject/Logic/NextGeneration.swift
+++ FinalProject/Logic/NextGeneration.swift
@@ -24,8 +24,11 @@ func NextGen(startPop: [[Int]], fertility: Int, monogamy: Bool, numOfKid: Int) -
         for _ in 0 ... (((femalePop.count-1)*fertility) / 100){
             let femaleParent = Int.random(in: 0 ... femalePop.count-1)
             let maleParent = Int.random(in: 0 ...  malePop.count-1)
-            for _ in (0 ... Int.random(in: 0 ... (numOfKid*2 - 1))){
-                secondGen.append([Int.random(in: 0 ... 1), femalePop[femaleParent][Int.random(in: 0 ... 1)], malePop[maleParent][Int.random(in: 0 ... 1)]])
+            let kids: Int = Int.random(in: 0 ... (numOfKid*2))
+            if kids != 0{
+                for _ in (1 ... kids){
+                    secondGen.append([Int.random(in: 0 ... 1), femalePop[femaleParent][Int.random(in: 0 ... 1)], malePop[maleParent][Int.random(in: 0 ... 1)]])
+                }
             }
             //1st element is sex of baby, 2nd element is allele from mother, 3rd element is allele from father
             femalePop.remove(at: femaleParent)
