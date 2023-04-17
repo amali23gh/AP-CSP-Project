35b74eb759de4a4f0cd103f1b0750b97099dcbfe
Russan-bot-9 <tpfechtmeye23@blakeschool.org>
2023-04-17 09:27:57 -0500

    1234

--- FinalProject/View/ContentView.swift
+++ FinalProject/View/ContentView.swift
@@ -11,84 +11,97 @@ struct ContentView: View {
     
     @State var simulationStart: Bool = false
     @State var simulationSTS: Bool = false
+    @State var simulationPD: Bool = false
        
        var body: some View {
         
            NavigationView {
-               
                ZStack {
-                   VStack{
-                       VStack {
-                           Text("Biology Simulations")
-                               .font(.title)
-                               .fontWeight(.bold)
-                               
-                           
-                           ButtonComponent(text: "Let's Begin")
-                               .onTapGesture{
-                                   simulationStart.toggle()
-                               }
-                           
-                           if simulationStart == true {
-                               VStack {
-                                   ZStack {
-                                       VStack{
-                                           HStack {
-                                               
-                                               Image(systemName:"arrowshape.turn.up.right.fill")
-                                                   .foregroundColor(.gray)
-                                               
-                                               Text("Single Trait Simuation")
-                                                   .font(.title2)
-                                                   .foregroundColor(.gray)
-                                                   .onTapGesture {
-                                                       simulationSTS.toggle()
-                                               
-                                                   }
-                                               
-                                           } .padding()
-                                           
-                                           if simulationSTS == true {
-                                               Text(" - Simulates Natural Selection of                Single Trait Dominance")
-                                                   .multilineTextAlignment(.center)
-                                               
-                                               NavigationLink(destination: SingleTraitSimulation()) {
-                                                   ButtonComponent(text: "Start")
-                                               } .offset(y: -5)
-                                           }
-                                           
-                                       }
-                                   } .frame(width: .infinity, height: .infinity)
-                                       .background(.white)
-                                       .cornerRadius(10)
-                                       .shadow(radius: 15)
-                                  
-                               ZStack {
-                                   VStack {
-                                       HStack {
-                                           
-                                           Image(systemName:"arrowshape.turn.up.right.fill")
-                                               .foregroundColor(.gray)
-                                           
-                                           Text(" Prisoner's Dilemma ")
-                                               .font(.title2)
-                                               .foregroundColor(.gray)
-                                           }
-                                       }
-                                   } .frame(width: .infinity, height: .infinity)
-                                       .background(.white)
-                                       .cornerRadius(5)
-                                       .shadow(radius: 15)
-                               }
-                           }
-                       }
-                   }
+                   Text("Biology Simulations")
+                       .font(.title)
+                       .fontWeight(.bold)
+                       .offset(y:-300)
+                    VStack{
+                        if simulationStart == false {
+                            ButtonComponent(text: "Let's Begin")
+                                .onTapGesture{
+                                    simulationStart.toggle()
+                                }
+                        }else{
+                            VStack {
+                                VStack{
+                                    HStack {
+                                        if simulationSTS == false{
+                                            Image(systemName:"chevron.right")
+                                                .foregroundColor(.gray)
+                                        }else{
+                                            Image(systemName:"chevron.down")
+                                                .foregroundColor(.gray)
+                                        }
+                                        Text("Single Trait Simuation")
+                                            .font(.title2)
+                                            .foregroundColor(.black)
+                                    }
+                                        .frame(width: 250, alignment: .leading)
+                                        .padding()
+                                        .onTapGesture {
+                                        simulationSTS.toggle()
+                                    }
+                                    if simulationSTS == true {
+                                        Text(" - Simulates Natural Selection of                Single Trait Dominance")
+                                            .multilineTextAlignment(.center)
+                                        
+                                        NavigationLink(destination: SingleTraitSimulation()) {
+                                            ButtonComponent(text: "Start")
+                                        } .offset(y: -5)
+                                    }
+                                }
+                                .frame(width: 300, height: .infinity)
+                                .background(.white)
+                                .cornerRadius(10)
+                                .shadow(radius: 15)
+                                VStack {
+                                    HStack {
+                                        if simulationPD == false{
+                                            Image(systemName:"chevron.right")
+                                                .foregroundColor(.gray)
+                                        }else{
+                                            Image(systemName:"chevron.down")
+                                                .foregroundColor(.gray)
+                                        }
+                                            
+                                        
+                                        Text(" Prisoner's Dilemma ")
+                                            .font(.title2)
+                                            .foregroundColor(.black)
+                                    }
+                                    .frame(width: 250, alignment: .leading)
+                                    .padding()
+                                        .onTapGesture {
+                                            simulationPD.toggle()
+                                        }
+                                    if simulationPD == true {
+                                        Text(" - Simulates game theory princeples in nature")
+                                            .multilineTextAlignment(.center)
+                                        
+                                        NavigationLink(destination: AnimalChoice()){
+                                            ButtonComponent(text: "Start")
+                                        } .offset(y: -5)
+                                    }
+                                    }
+                                .frame(width: 300, height: .infinity)
+                                .background(.white)
+                                .cornerRadius(10)
+                                .shadow(radius: 15)
+                            }
+                        }
+                    }
                }.frame(maxWidth: .infinity, maxHeight: .infinity)
                    .padding()
                    .ignoresSafeArea()
                    .background(Color(red: 0.91, green: 0.76, blue: 0.66))
            }
-          
+           .navigationBarBackButtonHidden()
        }
    }
 
