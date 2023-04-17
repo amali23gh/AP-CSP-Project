c3f4dd375049eeda01e8e677cfdb535f8219fae9
Russan-bot-9 <tpfechtmeye23@blakeschool.org>
2023-04-17 16:23:09 -0500

    DNA builder Half done

--- FinalProject/Component/DNAveiw53.swift
+++ FinalProject/Component/DNAveiw53.swift
@@ -11,36 +11,66 @@ struct DNAveiw53: View {
     var size: CGFloat
     var DNAseq: String
     var body: some View{
-        HStack(spacing: -78*size){
-            ForEach(Array(DNAseq), id: \.self){ i in
-                if i == "A" || i == "a"{
-                   Image("Abase")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 100*size)
-                }else if i == "T" || i == "t"{
-                    Image("Tbase")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 100*size)
-                }else if i == "C" || i == "c"{
-                    Image("Cbase")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 100*size)
-                }else if i == "G" || i == "g"{
-                    Image("Gbase")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 100*size)
+        HStack(alignment: .top, spacing: 5*size){
+            Text("5'")
+                .font(.custom("AmericanTypewriter",
+                    fixedSize: 5*size))
+            HStack(alignment: .top, spacing: -1.1*size){
+                ForEach(Array(DNAseq), id: \.self){ i in
+                    if i == "A" || i == "a"{
+                        ZStack{
+                            Image("Abase")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 10*size)
+                            Text("A")
+                                .font(.custom("AmericanTypewriter",
+                                              fixedSize: 4*size))
+                        }
+                    }else if i == "T" || i == "t"{
+                        ZStack{
+                            Image("Tbase")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 10*size)
+                            Text("T")
+                                .font(.custom("AmericanTypewriter",
+                                              fixedSize: 4*size))
+                        }
+                    }else if i == "C" || i == "c"{
+                        ZStack{
+                            Image("Cbase")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 10*size)
+                            Text("C")
+                                .font(.custom("AmericanTypewriter",
+                                              fixedSize: 4*size))
+                        }
+                        
+                    }else if i == "G" || i == "g"{
+                        ZStack{
+                            Image("Gbase")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 10*size)
+                            Text("G")
+                                .font(.custom("AmericanTypewriter",
+                                              fixedSize: 4*size))
+                            
+                        }
+                    }
                 }
             }
+            Text("3'")
+                .font(.custom("AmericanTypewriter",
+                    fixedSize: 5*size))
         }
     }
 }
 
 struct DNAveiw53_Previews: PreviewProvider {
     static var previews: some View {
-        DNAveiw53(size: 2, DNAseq: "AAAAATGATGACAGTACAGATGAC")
+        DNAveiw53(size: 4, DNAseq: "Atgtgtgtacac")
     }
 }
--- FinalProject/View/DNAbuilder.swift
+++ FinalProject/View/DNAbuilder.swift
@@ -10,20 +10,31 @@ import SwiftUI
 struct DNAbuilder: View {
     @State var DNAseq: String = ""
     var body: some View {
-        ZStack{
+        ScrollView{
+         VStack(alignment: .center){
             Text("DNA SIMULATION")
                 .font(.custom(
                     "AmericanTypewriter",
                     fixedSize: 36))
-                .offset(y:-350)
-            TextField("Enter DNA Sequence",text: $DNAseq)
+            TextField("Enter DNA Sequence",text: $DNAseq,axis: .vertical)
                 .padding()
                 .frame(width:350, height: .infinity)
                 .background(Color(red: 0.90, green: 0.80, blue: 0.84))
-                .offset(y:-300)
-            ScrollView(.horizontal){
-                DNAveiw53(size: 2, DNAseq: DNAseq)
+            VStack{
+                Text("Coding Strand")
+                    .font(.custom(
+                        "AmericanTypewriter",
+                        fixedSize: 20))
+                ScrollView(.horizontal){
+                    DNAveiw53(size: 5, DNAseq: DNAseq)
+                    
+                }
+                .frame(width: .infinity, height: 100)
+                .shadow(radius: 10)
             }
+            .padding()
+            .background(Color(red: 0.90, green: 0.80, blue: 0.84))
+        }
         }
     }
 }
