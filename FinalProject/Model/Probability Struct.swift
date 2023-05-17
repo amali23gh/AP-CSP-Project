b4dd16302c4ecf3e7d7ef3151da3c91112022f36
Russan-bot-9 <tpfechtmeye23@blakeschool.org>
2023-05-16 21:08:53 -0500

    a

--- FinalProject/Model/Probability Struct.swift
+++ FinalProject/Model/Probability Struct.swift
@@ -2,7 +2,7 @@
 
 import Foundation
 
-// Logic File Name: Probability Struct (for Animal's Dliemma)
+// Logic File Name: Probability Struct (for Animal's Dliemma) this file contains modle data for other parts of the program. in general our application doesnt use that much long term stored varriables for a model but it does use some. this file contributes to the models section of our MVC structure. 
 
 struct Prob {
     static var animalList: [String] = ["Zebra","Lion","Meerkat","Vulture","Hyena"]
